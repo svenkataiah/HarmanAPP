012af5a0e02f00d8b211f1ae19138d5098f7546b
svenkataiah <39156812+svenkataiah@users.noreply.github.com>
2018-07-02 12:59:15 +0530

    admin app
    initial setup

---
+++ BrowserApp/src/app/shared/services/signup.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  signup(data) {
+    return this.http.post("", JSON.stringify(data));
+  }
+}
